62708128fe5384280bf9cd2817d2d62b2fe1353b
susumu tomita <oyster880@gmail.com>
2024-04-21 09:53:08 +0900

    update actions

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,3 @@
-//!
-//! Stylus Hello World
-//!
-//! The following contract implements the Counter example from Foundry.
-//!
-//! ```
-//! contract Counter {
-//!     uint256 public number;
-//!     function setNumber(uint256 newNumber) public {
-//!         number = newNumber;
-//!     }
-//!     function increment() public {
-//!         number++;
-//!     }
-//! }
-//! ```
-//!
-//! The program is ABI-equivalent with Solidity, which means you can call it from both Solidity and Rust.
-//! To do this, run `cargo stylus export-abi`.
-//!
-//! Note: this code is a template-only and has not been audited.
-//!
-
 // Allow `cargo stylus export-abi` to generate a main function.
 #![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
@@ -38,6 +15,7 @@ sol_storage! {
     #[entrypoint]
     pub struct Counter {
         uint256 number;
+        address owner;
     }
 }
 
